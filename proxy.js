0ef0acbb6a7f1a580d381d98c0874a1bd6f3a4b6
Alexander Yurkevich <alexander.yurkevich@vrpconsulting.com>
2018-09-12 17:40:03 +0300

    adsa sd

--- proxy.js
+++ proxy.js
@@ -1,1 +1,13 @@
-alert('aaaa');
+const http = require('http')
+const port = 80
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
+}
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
