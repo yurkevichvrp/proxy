4c9f2e358950b8aed83e611592a81d81257ccc4f
Alexander Yurkevich <alexander.yurkevich@vrpconsulting.com>
2018-09-12 17:16:20 +0300

    2133

--- proxy.js
+++ proxy.js
@@ -1,9 +1,1 @@
-var http = require ('http');
-
-http.get ({
-    host: 'my.proxy.com',
-    port: 8080,
-    path: 'http://nodejs.org/'
-}, function (response) {
-    console.log (response);
-});
+alert('aaaa');
