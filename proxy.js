0747b5e3772fd75582ac15779d393511df41f421
Alexander Yurkevich <alexander.yurkevich@vrpconsulting.com>
2018-09-12 17:14:30 +0300

    aawd

--- proxy.js
+++ proxy.js
@@ -1,23 +1,9 @@
-http.createServer(onRequest).listen(80);
+var http = require ('http');
 
-function onRequest (client_req, client_res) {
-    client_req.addListener("end", function() {
-        var options = {
-            hostname: 'www.google.com',
-            port: 80,
-            path: client_req.url,
-            method: 'GET'
-        };
-        var req=http.request(options, function(res) {
-            var body;
-            res.on('data', function (chunk) {
-                body += chunk;
-            });
-            res.on('end', function () {
-                 client_res.writeHead(res.statusCode, res.headers);
-                 client_res.end(body);
-            });
-        });
-        req.end();
-    });
-}
+http.get ({
+    host: 'my.proxy.com',
+    port: 8080,
+    path: 'http://nodejs.org/'
+}, function (response) {
+    console.log (response);
+});
