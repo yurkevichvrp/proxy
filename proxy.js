3917727ae9f79485cfd2847a18cdf677ff520d02
Alexander Yurkevich <alexander.yurkevich@vrpconsulting.com>
2018-09-13 10:54:30 +0300

    aa

--- proxy.js
+++ proxy.js
@@ -1,19 +1,19 @@
 const http = require('http')
 var port = process.env.PORT || 5000;
 const requestHandler = (request, response) => {
-console.log(request.url)
-response.end(request.url)
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
 }
 const server = http.createServer(requestHandler)
 server.on('request', (req, res) => {
-var connector = http.request({
-host: '',
-path: req.url,
-method: req.method,
-headers: req.headers
-}, (resp) => {
-resp.pipe(res);
-});
+  var connector = http.request({
+    host: ,
+    path: req.url,
+    method: req.method,
+    headers: req.headers
+  }, (resp) => {
+    resp.pipe(res);
+  });
 
-req.pipe(connector);
+  req.pipe(connector);
 });
